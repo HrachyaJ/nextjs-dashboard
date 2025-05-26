4a025fbf54c56a794732267dfdb682ae9f24f8c9
Hrachya <hrachjgshow@gmail.com>
2025-05-26 15:50:45 +0400

    Second commit

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+
+// Experimental features can be enabled in the next.config.js file
+// and can be used to enable features that are not yet stable or are in beta.
+// For example, the `ppr` feature can be enabled to use the new partial page rendering feature.
+// export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
